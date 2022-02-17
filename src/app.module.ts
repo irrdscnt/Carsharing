73eacc888cb3b934cc069312f2ba33c33c1921fd
Aikanysh <you@example.com>
2022-02-17 20:19:15 +0600

    added mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.ma22l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
