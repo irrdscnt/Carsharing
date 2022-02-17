e3e303c2436436e5032fbba35ce6d01f73dbd13e
mainazarov1 <mainazarov1@gmail.com>
2022-02-17 20:41:47 +0600

    feat: add mongoose to package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.ma22l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-  , OrderModule],
+		MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.ma22l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+		OrderModule
+	],
   controllers: [],
   providers: [],
 })
