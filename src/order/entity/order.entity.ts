763dccd2b89499163bfd819e04eff70f53993450
mainazarov1 <mainazarov1@gmail.com>
2022-02-18 01:11:22 +0600

    refactor: remove unused files

--- src/order/entity/order.entity.ts
+++ src/order/entity/order.entity.ts
@@ -1,38 +1,36 @@
-import { Prop, Schema } from "@nestjs/mongoose";
-import { IOrder } from "../interface/";
+import { Prop, Schema } from '@nestjs/mongoose';
+import { IOrder } from '../interface/';
 
 @Schema()
-export class OrderEntity implements IOrder{
-    @Prop({
-        type:String,
-        required:true,
-    })
-    name:string;
-    @Prop({
-        type:String,
-        required:true,
-    })
-    phone:string;
-    @Prop({
-        type:String,
-        required:true,
-    })
-    startDate:string;
-    @Prop({
-        type:String,
-        required:true,
-    })
-    endDate:string;
-    @Prop({
-        type:String,
-        required:true,
-    })
-    brand:string;
-    @Prop({
-        type:Number,
-        required:true,
-    })
-    totalPrice: number;
-
-
+export class OrderEntity implements IOrder {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  name: string;
+  @Prop({
+    type: String,
+    required: true,
+  })
+  phone: string;
+  @Prop({
+    type: String,
+    required: true,
+  })
+  startDate: string;
+  @Prop({
+    type: String,
+    required: true,
+  })
+  endDate: string;
+  @Prop({
+    type: String,
+    required: true,
+  })
+  brand: string;
+  @Prop({
+    type: Number,
+    required: true,
+  })
+  totalPrice: number;
 }
--- src/order/interface/order.interface.ts
+++ src/order/interface/order.interface.ts
@@ -1,13 +1,13 @@
-import { SchemaFactory } from "@nestjs/mongoose";
-import { OrderEntity } from "../entity";
+import { SchemaFactory } from '@nestjs/mongoose';
+import { OrderEntity } from '../entity';
 
-export interface IOrder{
-    name:string,
-    phone:string,
-    startDate:string,
-    endDate:string,
-    brand:string,
-    totalPrice:number,
+export interface IOrder {
+  name: string;
+  phone: string;
+  startDate: string;
+  endDate: string;
+  brand: string;
+  totalPrice: number;
 }
 
-export const OrderSchema=SchemaFactory.createForClass(OrderEntity);
+export const OrderSchema = SchemaFactory.createForClass(OrderEntity);
