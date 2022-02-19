4ddf4fd0f9fbabb67671c0d2e63795e275c443fb
mainazarov1 <mainazarov1@gmail.com>
2022-02-19 17:14:56 +0600

    feat: add scripts

---
+++ src/order/scripts/scripts.ts
@@ -0,0 +1,17 @@
+let cars = [
+  {
+    brand: 'BMW',
+    price: 100,
+    isAvailable: true,
+  },
+  {
+    brand: 'Toyota',
+    price: 70,
+    isAvailable: true,
+  },
+  {
+    brand: 'Ford',
+    price: 60,
+    isAvailable: true,
+  },
+];
